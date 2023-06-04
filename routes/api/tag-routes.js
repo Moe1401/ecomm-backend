383f3d9f59e76e1180db96ab07413630e5949f21
Mylan <moe1401@gmail.com>
2023-06-04 17:48:49 -0500

    init upload

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try{
+    res.status(200).json(await Tag.findAll({ include: [{model: Product, through: ProductTag}]}));
+    } catch (err){res.status(500).json(err)}
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  if (!req.params.id) {
+    res.status(404).json({message: "Ope! check the id!"});
+    return;
+  }
+  try{
+    res.status(200).json(await Tag.findByPk(req.params.id, { include : [{model: Product, through: ProductTag}]}));
+    } catch (err){res.status(500).json(err)}
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try{
+    res.status(200).json(await Tag.create(req.body));
+    } catch (err){res.status(400).json(err)}
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  if (!req.params.id) {
+    res.status(404).json({message: "Ope! check the id!"});
+    return;
+  }
+  try{
+    res.status(200).json(await Tag.update(req.body, { where: {id: req.params.id},}));
+    } catch (err){res.status(500).json(err)}
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  if (!req.params.id) {
+    res.status(404).json({message: "Ope! check the id!"});
+    return;
+  }
+  try{
+    res.status(200).json(await Tag.destroy( {where: {id: req.params.id}}));
+    } catch (err){res.status(500).json(err)}
+});
+
+module.exports = router;
